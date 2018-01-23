4540ddea742926e15ada5238cb88ea7334e57e0c
Joakim Moss Grutle <joakim.grutle@student.uib.no>
2018-01-23 14:49:03 +0100

    Mer kart info

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function initMap(){
   var akvariet = {lat: 60.399776, lng: 5.303468};
   var bryggen = {lat: 60.397625, lng: 5.324571};
+  var floyen = {lat: 60.398801, lng: 5.345680};
+  var ulriken = {lat: 60.377952, lng: 5.386695};
+  var fisketorget = {lat: 60.394942, lng: 5.325351};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: bryggen
+    zoom: 13,
+    center: fisketorget
   });
   var marker1 = new google.maps.Marker({
     position: akvariet,
@@ -11,6 +14,18 @@ function initMap(){
   });
   var marker2 = new google.maps.Marker({
     position: bryggen,
+    map: map
+  })
+  var marker3 = new google.maps.Marker({
+    position: floyen,
+    map: map
+  })
+  var marker4 = new google.maps.Marker({
+    position: ulriken,
+    map: map
+  })
+  var marker5 = new google.maps.Marker({
+    position: fisketorget,
     map: map
   })
 }
