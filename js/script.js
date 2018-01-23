42c54d5e8ac812438231c2b5fcd2da4ba8bc11ec
Joakim Moss Grutle <joakim.grutle@student.uib.no>
2018-01-23 14:12:20 +0100

    endret på header

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function initMap(){
   var akvariet = {lat: 60.399776, lng: 5.303468};
   var bryggen = {lat: 60.397625, lng: 5.324571};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 11,
     center: bryggen
   });
   var marker1 = new google.maps.Marker({
